b299db7e777674d4b459486488b3d059e0704b14
Valavi, Roozbeh (Environment, Clayton) <Roozbeh.Valavi@csiro.au>
2024-08-27 15:27:29 +1000

    authors

--- R/ClassicHCAS.R
+++ R/ClassicHCAS.R
@@ -1,10 +1,9 @@
 #' ClassicHCAS: The Classic Habitat Condition Assessment System (HCAS)
 #'
-#' The HCAS (Habitat Condition Assessment System) benchmarking function evaluates habitat
-#' condition by comparing observed and predicted remote sensing (RS) variables. It integrates
-#' multiple RS data layers to provide a comprehensive assessment of habitat quality and changes
-#' over time. This function is designed to help researchers and conservationists quantify the
-#' impacts of environmental changes and management interventions on habitat condition.
+#' The HCAS evaluates habitat condition by comparing observed and predicted
+#' remote sensing (RS) variables using reference ecosystem samples.
+#' It integrates multiple RS data layers to provide a comprehensive assessment of
+#' habitat quality.
 #'
 #' @seealso \code{\link{benchmark}} and \code{\link{histogram}}.
 #'
@@ -12,6 +11,6 @@
 #'
 #' @name ClassicHCAS
 #' @docType _PACKAGE
-#' @author Roozbeh Valavi, el al. (will be updated)
+#' @author Roozbeh Valavi, Chris Ware, Eric Lehmann, Kristen Williams, Mike Birchall, Simon Collings, Simon Ferrier, and Tom Harwood
 #' @import Rcpp
 NULL
