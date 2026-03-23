915d4116299e5aa1c11d78ba3e7ea9068b94a9b7
Roozbeh Valavi <valavi.r@gmail.com>
2026-03-23 15:04:05 +1100

    Remove build artifacts from repository

--- ClassicHCAS.Rcheck/00_pkg_src/ClassicHCAS/R/ClassicHCAS-package.R
+++
@@ -1,5 +0,0 @@
-## usethis namespace: start
-#' @useDynLib ClassicHCAS, .registration = TRUE
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
--- ClassicHCAS.Rcheck/00_pkg_src/ClassicHCAS/R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-    packageStartupMessage("ClassicHCAS ", utils::packageVersion("ClassicHCAS"))
-}
